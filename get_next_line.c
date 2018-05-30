68cdbb865785d38f0db390cdd58f23b8463f7263
Victoria Lee <vlee@e1z3r3p10.42.us.org>
2018-05-29 19:17:48 -0700

    passed basic tests and fixed all leaks

--- get_next_line.c
+++ get_next_line.c
@@ -21,24 +21,28 @@ int	get_next_line(const int fd, char **line)
 	char		*match_word;
 	//char		tmp[BUFF_SIZE + 1];
 	char		*tmp;
+	char		*free_tmp;
 	//static char storage[BUFF_SIZE + 1];
 	//char buf[BUF_SIZE + 1];
 
 	static char *storage = NULL;
 	if (storage == NULL)
 		storage = ft_strnew(BUFF_SIZE);
-	//ft_bzero(tmp, ft_strlen(tmp));
-	//checks for errors
 	if (fd < 0 || line == NULL || read(fd, storage, 0) < 0)
 		return (-1);
 	*line = NULL;
-	//tmp = NULL;
+	tmp = NULL;
 	while (!ft_strchr(storage, '\n'))
 	{
 		if (!storage[0])
 		{
 			if (!read(fd, storage, BUFF_SIZE))
-				return (0);
+			{
+			 	if (*line)
+				 	return (1); //checks to see if line has content
+				else
+					return (0);
+			}
 		}
 		//it there's no \n in the current storage
 		if (!(match = ft_strchr((const char *)storage, '\n')))
@@ -46,10 +50,9 @@ int	get_next_line(const int fd, char **line)
 			//line already contains chars
 			if (*line)
 			{
-				ft_strcpy(tmp, *line);
-				//free(*line);
-				*line = ft_strnew(ft_strlen(*line) + ft_strlen(storage));
-				*line = ft_strcat(tmp, storage);
+				free_tmp = *line;
+				*line = ft_strjoin(*line, storage);
+				free(free_tmp);
 			}
 			//if line is null
 			else
@@ -57,62 +60,47 @@ int	get_next_line(const int fd, char **line)
 				*line = ft_strnew(ft_strlen(storage));
 				ft_strcpy(*line, storage);
 			}
-			//printf("storage while looking for nl contains: %s\n", storage);
 			ft_bzero(storage, ft_strlen(storage));
 		}
 	}
 		//if a \n is found in the current storage..
 		match = ft_strchr((const char *)storage, '\n');
-		match_word = ft_strsub((const char *)storage, 0, match - storage); //when new line is first thing read in storage, match_word = 0
-		//match_word[ft_strlen(match_word)] = '\0'; //is this adding a null???
+		match_word = ft_strsub((const char *)storage, 0, match - storage);
 		if (*line)
 		{
-			tmp = ft_strdup(*line);
-			//ft_strcpy(tmp, *line);
-			//free(*line);
-			*line = ft_strnew(ft_strlen(*line) + (match - storage));
-			*line = ft_strjoin(tmp, match_word);
-			//(*line)[ft_strlen(*line)] = '\0';
+			free_tmp = *line;
+			*line = ft_strjoin(*line, match_word);
+			free(free_tmp);
+
 		}
 		else
-			//*line = ft_strnew(match - storage);
 			*line = match_word;
-		// if (match_word == NULL) //trying to say if match_word is "" 		//Q: Why does this not work?
-		// 	ft_memmove(storage, match + 1, 1);
-		//printf("storage before memmove contains: %s\n", storage);
-		// printf("1st char before memmove: %c\n", storage[0]);
-		// printf("2nd char before memmove: %c\n", storage[1]);
-		// printf("3rd char before memmove: %c\n", storage[2]);
-		// printf("4th char before memmove: %c\n", storage[3]);
-		if (match - storage == 0) //this works to increment the pointer by 1!!! may not need lines 74-75
+		if (match - storage == 0)
 			ft_memmove(storage, match + 1, ft_strlen(match));
 		else
-			ft_memmove(storage, match + 1, ft_strlen(match - 1)); //when nl is first char,
-		//printf("storage after memmove contains: %s\n", storage);
-		// printf("1st char after memmove: %c\n", storage[0]);
-		// printf("2nd char after memmove: %c\n", storage[1]);
-		// printf("3rd char after memmove: %c\n", storage[2]);
-		// printf("4th char after memmove: %c\n", storage[3]);
-		//ft_bzero(tmp, ft_strlen(tmp));
+			ft_memmove(storage, match + 1, ft_strlen(match - 1));
 		ft_memdel((void *)&tmp);
-		//free(tmp);
+		ft_memdel((void *)&match_word);
 		return (1);
 }
-//do I need to free tmp?
+
 int	main(int argc, char **argv)
 {
 	int		fd;
 	char	*line;
 	int		ret;
+	int		lines;
 
 	fd = open(argv[1], O_RDONLY);
+	lines = 0;
 	if (fd == -1)
 		write(2, "error opening file", ft_strlen("error opening file"));
 	while ((ret = get_next_line(fd, &line)))
 	{
 		ft_putstr(line);
 		ft_putchar('\n');
-
+		lines++;
+		//free(line);
 		/*
 		printf("ret: %d\n", ret);
 		if (ret == -1)
@@ -122,5 +110,8 @@ int	main(int argc, char **argv)
 		}
 		printf("the line that will be printed: %s\n", line);*/
 	}
+	printf("total lines printed: %d\n", lines);
+	while(1);
+	// leaks();
 	return (0);
 }
