d4a3cdc5ad2fcb2cddef4bbc4e4fcb9c292cac5f
Victoria Lee <vlee@e1z3r3p12.42.us.org>
2018-06-01 20:13:14 -0700

    added multiple fd reader and fixed last bugs

--- get_next_line.c
+++ get_next_line.c
@@ -15,103 +15,132 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// char 	*readit(fd)
+// {
+// 	if (!storage[fd][0])
+// 	{
+// 		if (!read(fd, storage[fd], BUFF_SIZE))
+// 		{
+// 			if (*line)
+// 				return (1); //checks to see if line has content
+// 			else
+// 				return (0);
+// 		}
+// 	}
+// }
+
+static void	copy_before_match(char **line, char **storage, const int fd)
+{
+	char		*free_tmp;
+
+	if (*line)
+	{
+		free_tmp = *line;
+		*line = ft_strjoin(*line, storage[fd]);
+		free(free_tmp);
+	}
+	else
+	{
+		*line = ft_strnew(ft_strlen(storage[fd]));
+		ft_strcpy(*line, storage[fd]);
+	}
+	ft_bzero(storage[fd], ft_strlen(storage[fd]));
+}
+
+// static void	copy_after_match(char **line, char *match_word)
+// {
+// 	char		*free_tmp;
+//
+// 	if (*line)
+// 	{
+// 		free_tmp = *line;
+// 		*line = ft_strjoin(*line, match_word);
+// 		free(free_tmp);
+// 	}
+// 	else
+// 		*line = match_word;
+// }
+
+static void	copy_after_match(char **line, char **storage, const int fd)
+{
+	char		*free_tmp;
+	char		*rest;
+
+	rest = ft_strsub((const char *)storage[fd], 0, match - storage[fd]);
+	if (*line)
+	{
+		free_tmp = *line;
+		*line = ft_strjoin(*line, rest);
+		free(free_tmp);
+	}
+	else
+		*line = rest;
+	ft_memdel((void *)&rest);
+}
+
 int	get_next_line(const int fd, char **line)
 {
 	char		*match;
-	char		*match_word;
-	//char		tmp[BUFF_SIZE + 1];
-	char		*tmp;
+	char		*rest;
 	char		*free_tmp;
-	//static char storage[BUFF_SIZE + 1];
-	//char buf[BUF_SIZE + 1];
+	static char *storage[9999];
 
-	static char *storage = NULL;
-	if (storage == NULL)
-		storage = ft_strnew(BUFF_SIZE);
-	if (fd < 0 || line == NULL || read(fd, storage, 0) < 0)
+	if (fd < 0 || line == NULL || fd > 9999 || read(fd, storage[fd], 0) < 0)
 		return (-1);
+	if (storage[fd] == NULL)
+		storage[fd] = ft_strnew(BUFF_SIZE);
 	*line = NULL;
-	tmp = NULL;
-	while (!ft_strchr(storage, '\n'))
+	while (!ft_strchr(storage[fd], '\n'))
 	{
-		if (!storage[0])
+		if (!storage[fd][0])
 		{
-			if (!read(fd, storage, BUFF_SIZE))
+			if (!read(fd, storage[fd], BUFF_SIZE))
 			{
 			 	if (*line)
-				 	return (1); //checks to see if line has content
+				 	return (1);
 				else
 					return (0);
 			}
 		}
-		//it there's no \n in the current storage
-		if (!(match = ft_strchr((const char *)storage, '\n')))
-		{
-			//line already contains chars
-			if (*line)
-			{
-				free_tmp = *line;
-				*line = ft_strjoin(*line, storage);
-				free(free_tmp);
-			}
-			//if line is null
-			else
-			{
-				*line = ft_strnew(ft_strlen(storage));
-				ft_strcpy(*line, storage);
-			}
-			ft_bzero(storage, ft_strlen(storage));
-		}
+		if (!(match = ft_strchr((const char *)storage[fd], '\n')))
+			copy_before_match(line, storage, fd);
 	}
-		//if a \n is found in the current storage..
-		match = ft_strchr((const char *)storage, '\n');
-		match_word = ft_strsub((const char *)storage, 0, match - storage);
+		match = ft_strchr((const char *)storage[fd], '\n');
+		rest = ft_strsub((const char *)storage[fd], 0, match - storage[fd]);
+		copy_after_match(&line, &storage, fd);
 		if (*line)
 		{
 			free_tmp = *line;
-			*line = ft_strjoin(*line, match_word);
+			*line = ft_strjoin(*line, rest);
 			free(free_tmp);
-
 		}
 		else
-			*line = match_word;
-		if (match - storage == 0)
-			ft_memmove(storage, match + 1, ft_strlen(match));
+			*line = rest;
+		if (match - storage[fd] == 0)
+			ft_memmove(storage[fd], match + 1, ft_strlen(match));
 		else
-			ft_memmove(storage, match + 1, ft_strlen(match - 1));
-		ft_memdel((void *)&tmp);
-		ft_memdel((void *)&match_word);
+			ft_memmove(storage[fd], match + 1, ft_strlen(match - 1));
+		//ft_memdel((void *)&rest);
 		return (1);
 }
 
-int	main(int argc, char **argv)
-{
-	int		fd;
-	char	*line;
-	int		ret;
-	int		lines;
-
-	fd = open(argv[1], O_RDONLY);
-	lines = 0;
-	if (fd == -1)
-		write(2, "error opening file", ft_strlen("error opening file"));
-	while ((ret = get_next_line(fd, &line)))
-	{
-		ft_putstr(line);
-		ft_putchar('\n');
-		lines++;
-		//free(line);
-		/*
-		printf("ret: %d\n", ret);
-		if (ret == -1)
-		{
-			printf("error - returning -1\n");
-			return (-1);
-		}
-		printf("the line that will be printed: %s\n", line);*/
-	}
-	printf("total lines printed: %d\n", lines);
-	while(1);
-	// leaks();
-	return (0);
-}
+// int	main(int argc, char **argv)
+// {
+// 	int		fd;
+// 	char	*line;
+// 	int		ret;
+// 	int		lines;
+//
+// 	fd = open(argv[1], O_RDONLY);
+// 	lines = 0;
+// 	if (fd == -1)
+// 		write(2, "error opening file", ft_strlen("error opening file"));
+// 	while ((ret = get_next_line(fd, NULL)))
+// 	{
+// 		ft_putstr(line);
+// 		ft_putchar('\n');
+// 		lines++;
+// 	}
+// 	printf("number of lines: %d\n", lines);
+// 	return (0);
+// }
