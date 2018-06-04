c4eb98ff8cd29c2378814625763502f71d84de56
Victoria Lee <Victoriaslee@gmail.com>
2018-06-04 12:36:59 -0700

    modularized get_next_line fcn

--- get_next_line.c
+++ get_next_line.c
@@ -15,74 +15,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// char 	*readit(fd)
-// {
-// 	if (!storage[fd][0])
-// 	{
-// 		if (!read(fd, storage[fd], BUFF_SIZE))
-// 		{
-// 			if (*line)
-// 				return (1); //checks to see if line has content
-// 			else
-// 				return (0);
-// 		}
-// 	}
-// }
-
-static void	copy_before_match(char **line, char **storage, const int fd)
+static void move_all_from_buffer(char **line, char *buf)
 {
 	char		*free_tmp;
 
-	if (*line)
-	{
-		free_tmp = *line;
-		*line = ft_strjoin(*line, storage[fd]);
-		free(free_tmp);
-	}
-	else
-	{
-		*line = ft_strnew(ft_strlen(storage[fd]));
-		ft_strcpy(*line, storage[fd]);
-	}
-	ft_bzero(storage[fd], ft_strlen(storage[fd]));
+	free_tmp = *line;
+	*line = ft_strjoin(*line, buf);
+	free(free_tmp);
+	ft_bzero(buf, ft_strlen(buf));
 }
 
-// static void	copy_after_match(char **line, char *match_word)
-// {
-// 	char		*free_tmp;
-//
-// 	if (*line)
-// 	{
-// 		free_tmp = *line;
-// 		*line = ft_strjoin(*line, match_word);
-// 		free(free_tmp);
-// 	}
-// 	else
-// 		*line = match_word;
-// }
-
-static void	copy_after_match(char **line, char **storage, const int fd)
+static int move_up_to_newline_from_buffer(char **line, char *buf)
 {
 	char		*free_tmp;
-	char		*rest;
+	char *i_newline;
+	char *what_to_copy;
 
-	rest = ft_strsub((const char *)storage[fd], 0, match - storage[fd]);
-	if (*line)
-	{
-		free_tmp = *line;
-		*line = ft_strjoin(*line, rest);
-		free(free_tmp);
-	}
+	i_newline = ft_strchr((const char *)buf, '\n');
+	if (!i_newline)
+		return (-1);
+	what_to_copy = ft_strsub((const char *)buf, 0, i_newline - buf);
+	free_tmp = *line;
+	*line = ft_strjoin(*line, what_to_copy);
+	free(free_tmp);
+	if (i_newline - buf == 0)
+		ft_memmove(buf, i_newline + 1, ft_strlen(i_newline));
 	else
-		*line = rest;
-	ft_memdel((void *)&rest);
+		ft_memmove(buf, i_newline + 1, ft_strlen(i_newline - 1));
+	ft_memdel((void *)&what_to_copy);
+	return (0);
 }
 
 int	get_next_line(const int fd, char **line)
 {
-	char		*match;
-	char		*rest;
-	char		*free_tmp;
+	char		*i_newline;
 	static char *storage[9999];
 
 	if (fd < 0 || line == NULL || fd > 9999 || read(fd, storage[fd], 0) < 0)
@@ -92,36 +58,14 @@ int	get_next_line(const int fd, char **line)
 	*line = NULL;
 	while (!ft_strchr(storage[fd], '\n'))
 	{
-		if (!storage[fd][0])
-		{
-			if (!read(fd, storage[fd], BUFF_SIZE))
-			{
-			 	if (*line)
-				 	return (1);
-				else
-					return (0);
-			}
-		}
-		if (!(match = ft_strchr((const char *)storage[fd], '\n')))
-			copy_before_match(line, storage, fd);
+		if (!storage[fd][0] && (!read(fd, storage[fd], BUFF_SIZE)))
+				return (*line) ? 1 : 0;
+		if (!(i_newline = ft_strchr((const char *)storage[fd], '\n')))
+			move_all_from_buffer(line, storage[fd]);
 	}
-		match = ft_strchr((const char *)storage[fd], '\n');
-		rest = ft_strsub((const char *)storage[fd], 0, match - storage[fd]);
-		copy_after_match(&line, &storage, fd);
-		if (*line)
-		{
-			free_tmp = *line;
-			*line = ft_strjoin(*line, rest);
-			free(free_tmp);
-		}
-		else
-			*line = rest;
-		if (match - storage[fd] == 0)
-			ft_memmove(storage[fd], match + 1, ft_strlen(match));
-		else
-			ft_memmove(storage[fd], match + 1, ft_strlen(match - 1));
-		//ft_memdel((void *)&rest);
-		return (1);
+	if (move_up_to_newline_from_buffer(line, storage[fd]) == -1)
+		return (-1);
+	return (1);
 }
 
 // int	main(int argc, char **argv)
@@ -132,10 +76,11 @@ int	get_next_line(const int fd, char **line)
 // 	int		lines;
 //
 // 	fd = open(argv[1], O_RDONLY);
+// 	printf("fd: %d\n", fd);
 // 	lines = 0;
 // 	if (fd == -1)
 // 		write(2, "error opening file", ft_strlen("error opening file"));
-// 	while ((ret = get_next_line(fd, NULL)))
+// 	while ((ret = get_next_line(fd, &line)))
 // 	{
 // 		ft_putstr(line);
 // 		ft_putchar('\n');
